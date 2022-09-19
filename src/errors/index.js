f8794c5414ce4c851af4be3fb0783c6ab5128c7c
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2022-09-19 11:40:30 +0100

    source clean out

--- src/errors/index.js
+++
@@ -1,9 +0,0 @@
-const notFound = (req, res) => res.status(404).json({msg: 'Route '+req.url+' Not found.'})
-
-const errorHandler = (err, req, res, next) => {
-	console.error(`🔥 ${err.message}`)
-	if (err instanceof CustomAPIError) { return res.status(err.statusCode).json({ msg: err.message }) }
-	res.status(500).json({ msg: err.message });
-}
-
-module.exports = { notFound, errorHandler }
